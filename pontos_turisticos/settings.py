20b273d6f0cddaecdc862ff22a3eb1197bd2b131
felipeqq <uluizfelipe@gmail.com>
2021-09-10 13:09:51 -0300

    .env

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-up*11+u@@udyd7_=!0o#h^m4&swmk(lwmdka+1tvf@gb8fd89x"
+SECRET_KEY = "SECRET_KEY_DJANGO"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
