5707c7617aba33674deae16140f850172caab41f
felipeqq <uluizfelipe@gmail.com>
2021-09-11 18:07:35 -0300

    @property e NestedRelationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,12 +1,37 @@
 from django.db.models import fields
+from rest_framework.fields import SerializerMethodField
 from core import models
 
 
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ("id", "nome", "descricao", "aprovado", "foto")
+        fields = (
+            "id",
+            "nome",
+            "descricao",
+            "aprovado",
+            "atracoes",
+            "comentarios",
+            "avaliacoes",
+            "endereco",
+            "foto",
+            "descricao_completa2",
+            "descricao_completa",
+            "endereco",
+            "atracoes",
+        )
+
+    def get_descricao_completa(self, obj):
+        return "%s - %s" % (obj.nome, obj.descricao)
